cc40f1b3ade5e943fa6cd91f8c186e230ce589f3
alemao00 <alisson.lepchack@escola.pr.gov.br>
2025-04-14 09:06:58 -0300

    inicio java script

---
+++ main.js
@@ -0,0 +1,10 @@
+let nome_usuario ="" ;
+let elemento = documenrr.querySelector ('#nome-usuario');
+while(nome_usuario = = ' ' ){
+    nome_usuario = prompt(Qual seu nome?);
+}
+if (nome_usuario = = null){
+    elemento.textContent = 'seja muito bem vido!';
+}else{
+    elemento.textContent = nome_usuario
+}
